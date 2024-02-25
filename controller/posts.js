56861f512449602c5b954c65a411df3e2a41e049
DanielKfir <danielkfir26@gmail.com>
2024-02-25 22:41:37 +0200

    added get25 posts and updated deletePost

--- controller/posts.js
+++ controller/posts.js
@@ -1,4 +1,7 @@
 const posts = require("../services/posts.js");
+const friends = require("../services/friends");
+const like = require("../services/like.js");
+const comment = require("../services/comment.js");
 
 const createPost = async (req, res) => {
     res.json(await posts.createPost(req.body.content, req.body.img, req.body.userId, req.body.date));
@@ -15,15 +18,40 @@ const updatePostContent = async (req, res) => {
 const updatePostImg = async (req, res) => {
     res.json(await posts.updatePostImg(req.params.id, req.params.img))
 }
-const deletePost = async (req, res) => {
-    res.json(await posts.deletePost(req.params.id))
-}
 const getAuthor = async (req, res) => {
     res.json(await posts.getAuthor(req.query.id))
 }
 const latestFivePost = async (req, res) => {
     res.json(await posts.latestFivepost());
 }
+/**
+ * func name: get25posts
+ * action: returns 20 latests posts of friends and 5 latests posts in general
+ * */
+const get25Posts = async (req, res) => {
+    var list = posts.latestFivePost();
+    list.concat(friends.getLastPostOfFriends());
+    res.json(list);
+
+}
+/**
+ * func name: deletePost
+ * action: delete post and its likes and comments.
+ * **/
+const deletePost = (req, res) => {
+    const post = posts.deletePost(req.params.postId);
+    like.deleteLikesByPost(req.params.postId);
+    comment.deleteCommentsByPost(req.params.postId)
+    res.json(post);
+}
 module.exports = {
-    createPost, getPostById, getPosts, updatePostContent, updatePostImg, deletePost, getAuthor, latestFivePost
+    createPost,
+    getPostById,
+    getPosts,
+    updatePostContent,
+    updatePostImg,
+    deletePost,
+    getAuthor,
+    latestFivePost,
+    get25Posts
 }
